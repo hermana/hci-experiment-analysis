a1baf6f08a91183ac5fddeeab836c6e04cccc799
hermana <aeherman@ualberta.ca>
2026-01-21 17:48:05 -0600

    read in for analysis

---
+++ game_mechanics_study/util/survey_structure.py
@@ -0,0 +1,15 @@
+
+FSS_SURVEY_STATEMENTS = {
+    'imi_interest_enjoyment': "I think this game is enjoyable.",
+    'imi_effort_importance': "I put a lot of effort into this game.",
+    'imi_value_usefulness': "I think this game could help me to learn the locations of icons in a real software interface.", 
+    'pens_competence': "My ability to play this game is well matched with the game's challenges.", 
+    'pens_intuitive_controls': "I am able to understand how to play the game.",
+    'attention': "Select the rightmost value (Strongly Agree). This is an attention check.",
+    'fss_time_transformation_quickly': "Time goes by quickly while I play this game.",
+    "fss_time_transformation_slowly": "Time goes by very slowly while I play this game.",
+    "fss_unambiguous_feedback": "I am aware of how well I am doing in the game",
+    "fss_concentration": "It doesn't take a lot of effort to concentrate on the game.",
+    "fss_merging_action_and_awareness": "When clicking on icons, I act spontaneously and don't overthink things"
+}
+
--- generate_simon_game_distribution_details.py
+++ generate_simon_game_distribution_details.py
@@ -1,11 +1,12 @@
 # SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4 5 6"], 
-#     ["2 4 6 1 3 5"],
-#     ["3 6 2 5 1 4"],
-#     ["4 1 5 2 6 3"],
-#     ["5 3 1 6 4 2"],
-#     ["6 5 4 3 2 1"]
+#     # ["1 2 3 4 5 6"], 
+#     # ["2 4 6 1 3 5"],
+#     # ["3 6 2 5 1 4"],
+#     # ["4 1 5 2 6 3"],
+#     # ["5 3 1 6 4 2"],
+#     # ["6 5 4 3 2 1"]
 # ]
+
 
 # SIMON_GAME_DISTRIBUTION = [
 #     ["1 2 3 4"], 
@@ -17,14 +18,14 @@
 # ]
 
 
-SIMON_GAME_DISTRIBUTION = [
-    ["1 2 6"], 
-    ["2 4 3 5"],
-    ["3 6 2 1 4"],
-    ["4 1 5 2 3"],
-    ["5 3 1 6 4 2"],
-    ["6 5 4 3 2 1"]
-]
+# SIMON_GAME_DISTRIBUTION = [
+#     ["1 2 6"], 
+#     ["2 4 3 5"],
+#     ["3 6 2 1 4"],
+#     ["4 1 5 2 3"],
+#     ["5 3 1 6 4 2"],
+#     ["6 5 4 3 2 1"]
+# ]
 
 # 12 blocks, with 6 "targets" instead! 
 # Talk to Morgan tomorrow 
@@ -62,7 +63,7 @@ SIMON_GAME_DISTRIBUTION = [
 # ]
 
 
-FIRST_LEVEL_SUBSTRING_LENGTH = 2
+FIRST_LEVEL_SUBSTRING_LENGTH = 3
 RANGE = 8
 # It could be easier to analyze on a per target basis. We need the above structure to have the same beginning, middle, end. 
 
