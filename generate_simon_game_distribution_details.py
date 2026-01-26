eeae16f6b9e0b2682a4bac7d3b91c92c832ff2b9
hermana <aeherman@ualberta.ca>
2026-01-25 19:41:30 -0600

    more graphs

--- generate_simon_game_distribution_details.py
+++ generate_simon_game_distribution_details.py
@@ -1,104 +1,17 @@
-# SIMON_GAME_DISTRIBUTION = [
-#     # ["1 2 3 4 5 6"], 
-#     # ["2 4 6 1 3 5"],
-#     # ["3 6 2 5 1 4"],
-#     # ["4 1 5 2 6 3"],
-#     # ["5 3 1 6 4 2"],
-#     # ["6 5 4 3 2 1"]
-# ]
-
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4"], 
-#     ["2 4 6 1"],
-#     ["3 6 2 5"],
-#     ["4 1 5 2"],
-#     ["5 3 1 6"],
-#     ["6 5 4 3"]
-# ]
-
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 6"], 
-#     ["2 4 3 5"],
-#     ["3 6 2 1 4"],
-#     ["4 1 5 2 3"],
-#     ["5 3 1 6 4 2"],
-#     ["6 5 4 3 2 1"]
-# ]
-
-# 12 blocks, with 6 "targets" instead! 
-# Talk to Morgan tomorrow 
-# However, end of the sequence could mean end of the subsequence? or not?
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 3 5 8"],
-#     ["2 4 6 1 7"],
-#     ["3 2 7"],
-#     ["4 5 3 6 2"],
-#     ["5 7 4"],
-#     ["6 1 5 8 3"],
-#     ["7 6 2 4"],
-#     ["8 2 7 1"]
-# ]
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 3 5 8"],
-#     ["2 4 6 1 7"],
-#     ["3 2 7"],
-#     ["4 5 3 6 2"],
-#     ["5 7 4"],
-#     ["6 1 5 8 3"],
-#     ["7 6 2 4"],
-#     ["8 2 7 1"]
-# ]
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 5 3 6 2"],
-#     ["4 6 2 3 1"],
-#     ["5 4 1 6 3"],
-#     ["2 1 5 4"],
-#     ["3 4 1 2 5 6"],
-#     ["6 2 3 5"],
-# ]
-
 
 FIRST_LEVEL_SUBSTRING_LENGTH = 3
 RANGE = 8
-# It could be easier to analyze on a per target basis. We need the above structure to have the same beginning, middle, end. 
 
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4 5"],
-#     ["2 4 1 5 3"],
-#     ["3 1 4 2 5"],
-#     ["4 5 2 3 1"],
-#     ["5 3 1 2 4"]
-# ]
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4"],
-#     ["2 4 1 3"],
-#     ["3 1 4 2"],
-#     ["4 3 2 1"]
-# ]
-
-
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4 2"],
-#     ["2 4 1 3 4"],
-#     ["3 1 4 2 1"],
-#     ["4 3 2 1 3"]
-# ]
 
 # The other idea: vary the sequence lengths. 
-# SIMON_GAME_DISTRIBUTION = [
-#     ["1 2 3 4 5 6"], 
-#     ["2 4 6 1 3 5"],
-#     ["3 6 2 5 1 4"],
-#     ["4 1 5 2 6 3"],
-#     ["5 3 1 6 4 2"],
-#     ["6 5 4 3 2 1"]
-# ]
+SIMON_GAME_DISTRIBUTION = [
+    ["1 2 3 4 5 6"], 
+    ["2 4 6 1 3 5"],
+    ["3 6 2 5 1 4"],
+    ["4 1 5 2 6 3"],
+    ["5 3 1 6 4 2"],
+    ["6 5 4 3 2 1"]
+]
 
 # Start with a substring of 3 
 
@@ -112,7 +25,7 @@ def get_number_of_appearances():
             if index <= FIRST_LEVEL_SUBSTRING_LENGTH -1:
                 appearances[int(value)-1] += (length - FIRST_LEVEL_SUBSTRING_LENGTH)
             elif index > FIRST_LEVEL_SUBSTRING_LENGTH -1:
-                appearances[int(value)-1] += (length-index)
+                appearances[int(value)-1] += (length-index+1)
     return appearances
 
 
